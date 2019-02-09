e27571d013b5a7f32459059d95ff32714cff5d32
Mitsuhau Emoto <mthr1982@gmail.com>
2019-02-09 13:14:05 +0900

    fix

--- SwiftException/ViewController.swift
+++ SwiftException/ViewController.swift
@@ -19,13 +19,6 @@ class ViewController: UIViewController {
         
         self.addTableView()
         
-//        ExcBlock.execute({
-//            let temps = [0, 1, 2]
-//            print("\(temps[10])")
-//            // ブロックの中で起こる例外がキャッチできないまま落ちる
-//        }) { (exception) in
-//            print(exception)
-//        }
     }
     
     func addTableView(){
@@ -38,6 +31,8 @@ class ViewController: UIViewController {
         self.tableView.delegate = self
         self.tableView.register(UINib(nibName: "VanillaCell", bundle: nil),
                                 forCellReuseIdentifier: "VanillaCell")
+        self.tableView.tableFooterView = UIView()
+        self.tableView.backgroundColor = UIColor(white: 0.99, alpha: 1.0)
         self.view.addSubview(self.tableView)
     }
     
@@ -53,28 +48,38 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController: UITableViewDataSource, UITableViewDelegate{
+extension ViewController{
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+    // 配列の範囲外の要素にアクセスする
+    func demo1(){
+        ExcBlock.execute({
+            let temps = [0, 1, 2]
+            let temp = temps[10]
+            print("temp = \(temp)")
+        }) { (exception) in
+            print(exception)
+            self.alert = {
+                let alert = UIAlertController(title: "exception",
+                                              message: exception.description,
+                                              preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title: "OK",
+                                              style: UIAlertAction.Style.default,
+                                              handler:nil))
+                return alert
+            }()
+            if let alert = self.alert{
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "VanillaCell",
-                                                 for: indexPath)
-        cell.textLabel?.text = "row:\(indexPath.row), counter:\(counter)"
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
+    // TableViewの範囲外のセルを更新する
+    func demo2(){
         
-        counter += 1
+        self.counter += 1
         let ip = IndexPath(row: 100, section: 100)
-
-        self.tableView.exc_reloadRows(at: [ip], with: UITableView.RowAnimation.automatic) { (exception) in
-            print("[exc_reloadRows] exception:\(exception)")
-            
+        
+        self.tableView.exc_reloadRows(at: [ip], with: .automatic) { (exception) in
             self.alert = {
                 let alert = UIAlertController(title: "exception",
                                               message: exception.description,
@@ -90,6 +95,52 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate{
                 self.present(alert, animated: true, completion: nil)
             }
         }
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "デモ \(section + 1)"
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "VanillaCell",
+                                                 for: indexPath)
+        
+        var str: String = ""
+        if indexPath.section == 0{
+            str = "配列の範囲外の要素にアクセスする"
+        }else if indexPath.section == 1{
+            str = "範囲外のセルを更新する (tap:\(self.counter))"
+        }
+        
+        cell.textLabel?.text = str
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        let sec = indexPath.section
+        if sec == 0{
+            self.demo1()
+        }else if sec == 1{
+            self.demo2()
+        }
+        
     }
     
 }
